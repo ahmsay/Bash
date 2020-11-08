35f3c194125b98e3472fc3cd7e5309d2f05f8daf
ahmsay <ahmsay@gmail.com>
2020-11-08 12:44:32 +0300

    triangle

---
+++ conditionals/triangle.sh
@@ -0,0 +1,15 @@
+read x
+read y
+read z
+
+if [ $x -eq $y ]; then
+    if [ $x -eq $z ]; then
+        echo EQUILATERAL
+    else
+        echo ISOSCELES
+    fi
+elif [ $y -eq $z ]; then
+    echo ISOSCELES
+else
+    echo SCALENE
+fi
