2f5aa1d76bcaf8103b6715e45ef3c63ca7608d64
ahmsay <ahmsay@gmail.com>
2020-12-22 22:03:11 +0300

    four operations are improved

--- arithmetic/four_operations.sh
+++ arithmetic/four_operations.sh
@@ -1,6 +1,7 @@
-read a
-read b
-echo $((a + b))
-echo $((a - b))
-echo $((a * b))
+read -p 'Enter a: ' a
+read -p 'Enter b: ' b
+echo $((a + b)) # ((...)) represents arithmetic expression
+echo $(expr $a - $b) # This is just another way
+myvar=$((a * b)) # Assign result to a variable. DO NOT USE SPACES AROUND =
+echo $myvar
 echo $((a / b))
