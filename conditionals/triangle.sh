0efd4ab4136d95509cf08a603e08a5d5e81a21cb
ahmsay <ahmsay@gmail.com>
2020-12-23 22:23:22 +0300

    triangle is updated

--- conditionals/triangle.sh
+++ conditionals/triangle.sh
@@ -1,15 +1,18 @@
-read x
-read y
-read z
+read -p 'Enter x: ' x
+read -p 'Enter y: ' y
+read -p 'Enter z: ' z
 
-if [ $x -eq $y ]; then
-    if [ $x -eq $z ]; then
-        echo EQUILATERAL
+if [ $x -eq $y ]
+  then
+    if [ $x -eq $z ]
+    then
+      echo Equiliteral
     else
-        echo ISOSCELES
+      echo Isosceles
     fi
-elif [ $y -eq $z ]; then
-    echo ISOSCELES
+elif [ $y -eq $z ]
+  then
+    echo Isosceles
 else
-    echo SCALENE
+    echo Scalene
 fi
