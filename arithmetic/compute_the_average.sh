2e4eed341b876166c8461ee2c9baee93d57bfbe4
ahmsay <ahmsay@gmail.com>
2020-12-24 20:55:05 +0300

    compute the average problem

--- arithmetic/compute_the_average.sh
+++ arithmetic/compute_the_average.sh
@@ -1,4 +1,10 @@
-read n
-arr=($(cat))
-arr=${arr[*]}
+read -p "Enter the number of elements: " n
+echo "Enter the array:"
+readarray -n $n arr # arr = [1,2,3]
+arr=${arr[*]} # arr = "1 2 3" (represent the array with space seperated integers in a string)
 printf "%.3f" $(echo $((${arr// /+}))/$n | bc -l)
+#arr=${arr// /+} # arr = "1+2+3"
+#sum=$((arr)) # sum = "6"
+#printf "%.3f" $(echo $(($sum / $n)) | bc -l)
+#echo
+
