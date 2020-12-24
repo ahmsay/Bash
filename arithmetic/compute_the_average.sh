e5b87d8cddb8626a3cfb83c8fdc471eee469a76c
ahmsay <ahmsay@gmail.com>
2020-12-24 21:39:41 +0300

    compute the avg is fixed

--- arithmetic/compute_the_average.sh
+++ arithmetic/compute_the_average.sh
@@ -1,10 +1,13 @@
 read -p "Enter the number of elements: " n
 echo "Enter the array:"
 readarray -n $n arr # arr = [1,2,3]
-arr=${arr[*]} # arr = "1 2 3" (represent the array with space seperated integers in a string)
-printf "%.3f" $(echo $((${arr// /+}))/$n | bc -l)
-#arr=${arr// /+} # arr = "1+2+3"
-#sum=$((arr)) # sum = "6"
-#printf "%.3f" $(echo $(($sum / $n)) | bc -l)
-#echo
-
+sum=0
+i=0
+while [ $i -lt $n ]
+do
+  num=${arr[$i]}
+  sum=$((sum + num))
+  ((i += 1))
+done
+avg=$(echo "scale=3; $sum/$n" | bc -l)
+echo "Average is: $avg"
