c2d51f83b38d4494d5cea543bdc979106052fc9f
ahmsay <ahmsay@gmail.com>
2020-12-25 22:50:43 +0300

    fileremove

--- arithmetic/compute_the_average.sh
+++ arithmetic/compute_the_average.sh
@@ -2,12 +2,18 @@ read -p "Enter the number of elements: " n
 echo "Enter the array:"
 readarray -n $n arr # arr = [1,2,3]
 sum=0
-i=0
-while [ $i -lt $n ]
+for i in {0..$n}
 do
   num=${arr[$i]}
   sum=$((sum + num))
   ((i += 1))
 done
+#i=0
+#while [ $i -lt $n ]
+#do
+#  num=${arr[$i]}
+#  sum=$((sum + num))
+#  ((i += 1))
+#done
 avg=$(echo "scale=3; $sum/$n" | bc -l)
 echo "Average is: $avg"
