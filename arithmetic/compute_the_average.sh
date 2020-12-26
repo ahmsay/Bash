1b7ac6bf95e16ce63511903fc9de3f90dcd3be85
ahmsay <ahmsay@gmail.com>
2020-12-26 11:36:06 +0300

    compute the average is optimized

--- arithmetic/compute_the_average.sh
+++ arithmetic/compute_the_average.sh
@@ -2,12 +2,8 @@ read -p "Enter the number of elements: " n
 echo "Enter the array:"
 readarray -n $n arr # arr = [1,2,3]
 sum=0
-i=0
-while [ $i -lt $n ]
-do
-  num=${arr[$i]}
+for num in "${arr[@]}"; do
   sum=$((sum + num))
-  ((i += 1))
 done
 avg=$(echo "scale=3; $sum/$n" | bc -l)
 echo "Average is: $avg"
