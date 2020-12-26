661a74d9334c77bd7485a01fade83c2150897b0b
ahmsay <ahmsay@gmail.com>
2020-12-26 15:14:25 +0300

    array concat

--- arrays/concat.sh
+++ arrays/concat.sh
@@ -1,3 +1,4 @@
-array=($(cat))
-total=("${array[@]}" "${array[@]}" "${array[@]}")
-echo ${total[@]}
+arr1=(1 2 3)
+arr2=("a" "b" "c")
+total=(${arr1[@]} ${arr2[@]})
+echo "Total array is: "${total[@]}
