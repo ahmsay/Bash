ec77c588e7c43efb6966ee135bc6494df1a36df3
ahmsay <ahmsay@gmail.com>
2020-12-27 18:06:39 +0300

    awk nr nf is elaborated

--- awk/nr_nf.sh
+++ awk/nr_nf.sh
@@ -1,6 +1,11 @@
+echo 'Display with line numbers:'
 awk '{print NR,$0}' employees.txt
+echo
 # NR displays line numbers
 # $0 represents the whole line
-echo
+echo 'Display first and last line:'
 awk '{print $1,$NF}' employees.txt
+echo
 # NF holds the number of fields. in this case first and last field will be displayed
+echo 'Display line from 3 to 6 with line numbers:'
+awk 'NR==3, NR==6 {print NR,$0}' employees.txt
