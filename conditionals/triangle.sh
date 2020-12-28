8d52768c5190be4568b9a51dacb11e466f392d8f
Ahmet Say <ahmsay@gmail.com>
2020-12-28 09:40:03 +0300

    indentation fix

--- conditionals/triangle.sh
+++ conditionals/triangle.sh
@@ -3,16 +3,16 @@ read -p 'Enter y: ' y
 read -p 'Enter z: ' z
 
 if [ $x -eq $y ]
+then
+  if [ $x -eq $z ]
   then
-    if [ $x -eq $z ]
-    then
-      echo Equiliteral
-    else
-      echo Isosceles
-    fi
-elif [ $y -eq $z ]
-  then
+    echo Equiliteral
+  else
     echo Isosceles
+  fi
+elif [ $y -eq $z ]
+then
+  echo Isosceles
 else
-    echo Scalene
+  echo Scalene
 fi
