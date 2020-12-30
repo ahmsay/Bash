aa4bbd1643d40573e5927372e8996fd368c9869e
ahmsay <ahmsay@gmail.com>
2020-12-30 20:45:22 +0300

    cut bytes is elaborated

--- cut/bytes.sh
+++ cut/bytes.sh
@@ -1,5 +1,18 @@
-echo "List without ranges:" #(-b extracts specific bytes)
+echo "The file:"
+cat states.txt
+echo
+
+echo "List first three bytes:" #(-b extracts specific bytes)
 cut -b 1,2,3 states.txt
 echo
-echo "List with ranges:"
+
+echo "List 1-3 and 5-7th bytes:"
 cut -b 1-3,5-7 states.txt
+echo
+
+echo "List until the 3rd byte:"
+cut -b -3 states.txt
+echo
+
+echo "List after the 2nd byte:"
+cut -b 2- states.txt
