3df71bbc30b8ad9af387205d00e327feacd04733
ahmsay <ahmsay@gmail.com>
2021-01-01 19:21:02 +0300

    grep show match index

---
+++ grep/show_match_index.sh
@@ -0,0 +1,4 @@
+grep -o -b "the" file1.txt # Displays the matching position and the given pattern
+# Display format is:
+# starting index of the match : pattern
+# index starts from 0 and continues after the line break as well. (second line index won't start with 0)
