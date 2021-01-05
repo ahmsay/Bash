009e6d4f00c34baab82d627a995b88e05c769a00
ahmsay <ahmsay@gmail.com>
2021-01-05 20:25:33 +0300

    sed substitution command

---
+++ sed/replace.sh
@@ -0,0 +1,6 @@
+sed 's/unix/linux/g' example.txt
+# s = substitution
+# / = delimeter
+# g = globally
+# 's/search pattern/replacement string/'
+# This command above gives the output. It doesn't edit the file.
