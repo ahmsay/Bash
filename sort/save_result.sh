e5f53ac79d207ed3d44749226d68dc4dd8b8be05
ahmsay <ahmsay@gmail.com>
2021-01-07 21:29:57 +0300

    sort save result

---
+++ sort/save_result.sh
@@ -0,0 +1,2 @@
+sort cities.txt > cities_sorted.txt # Creates a file named cities_sorted.txt and saves the result
+# sort -o cities.txt cities_sorted.txt # This one is not that smart, we need the file in advance, otherwise it will fail
