41389eed7082599bcea81bb3fde51aa689172c8b
ahmsay <ahmsay@gmail.com>
2021-01-11 20:14:32 +0300

    tail advanced commands

---
+++ tail/advanced.sh
@@ -0,0 +1,5 @@
+tail -c -6 employees.txt # Prints last 6 characters of the file
+echo
+#tail -f employees.txt # See the last 10 lines (by default) of a file live. 
+echo
+tail -v employees.txt products.txt # Display file names along with content
