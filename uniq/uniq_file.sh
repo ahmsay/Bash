760c8f5b7a0557a44fe971e47f7ff915e9750b33
ahmsay <ahmsay@gmail.com>
2021-01-12 19:57:25 +0300

    uniq basic commands

--- uniq/uniq_file.sh
+++ uniq/uniq_file.sh
@@ -1,1 +1,9 @@
 uniq music.txt # All repeated lines are removed
+echo
+uniq -c music.txt # Displays the number of times a line was repeated aswell
+echo
+uniq -d music.txt # Displays only repeated lines
+echo
+uniq -D music.txt # Displays only repeated lines but not one per group
+echo
+uniq -u music.txt # Displays only unique lines
