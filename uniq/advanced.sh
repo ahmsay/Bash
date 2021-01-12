c6b37498998999f96b7a388ce9ffbb6c9b2eab1a
ahmsay <ahmsay@gmail.com>
2021-01-12 20:19:45 +0300

    uniq -w and -i commands

--- uniq/advanced.sh
+++ uniq/advanced.sh
@@ -2,3 +2,7 @@ uniq -f 2 music_numbered.txt # Skips first 2 fields while checking uniqueness (d
 echo
 uniq -s 1 music_numbered.txt # Similar to -f but skips characters, not fields
 echo
+uniq -w 3 questions.txt # Checks only first 3 letters for uniqueness
+echo
+uniq -i jazz.txt # Case insensitive check
+
