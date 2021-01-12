49dd71b8fb87808e21c2dd9786a299e497fbe59a
ahmsay <ahmsay@gmail.com>
2021-01-12 20:10:47 +0300

    uniq advanced commands

---
+++ uniq/advanced.sh
@@ -0,0 +1,4 @@
+uniq -f 2 music_numbered.txt # Skips first 2 fields while checking uniqueness (delimeter is whitespace)
+echo
+uniq -s 1 music_numbered.txt # Similar to -f but skips characters, not fields
+echo
