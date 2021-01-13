3f4a8791e4238d95ecf71243c702b694dedcbc53
ahmsay <ahmsay@gmail.com>
2021-01-13 19:23:01 +0300

    find file basic examples

---
+++ find/find_files.sh
@@ -0,0 +1,5 @@
+find . -name file.txt # Searches for file.txt in current directory
+echo
+find ./folder_1 -name info.txt # Searches for info.txt in folder_1
+echo
+find . -iname file.txt # Case insensitive search
