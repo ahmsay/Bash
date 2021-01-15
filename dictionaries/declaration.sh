a86e24d9503069d0a7a518724995cb59366ffe72
ahmsay <ahmsay@gmail.com>
2021-01-15 19:36:07 +0300

    declaration refactor

--- dictionaries/declaration.sh
+++ dictionaries/declaration.sh
@@ -1,4 +1,7 @@
-declare -A animals # By dictionaries I mean associative arrays
+# By dictionaries I mean associative arrays
+declare -A animals
 animals=(['cat']='meow' ['dog']='woof')
+# Or it can be defined like this: declare -A animals=(['cat']='meow' ['dog']='woof')
+
 echo ${animals[@]} # Prints all values
 echo ${!animals[@]} # Prints all keys
