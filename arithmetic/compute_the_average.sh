72422a59944b9cd4a1a88eeed72f8d6e42827c9c
ahmsay <ahmsay@gmail.com>
2021-01-27 20:19:21 +0300

    compute the average is simplified

--- arithmetic/compute_the_average.sh
+++ arithmetic/compute_the_average.sh
@@ -1,9 +1,11 @@
-read -p "Enter the number of elements: " n
-echo "Enter the array:"
-readarray -n $n arr # arr = [1,2,3]
+read -p 'Enter the array as space separated integers: ' line
+arr=($line)
+
 sum=0
 for num in "${arr[@]}"; do
   sum=$((sum + num))
 done
-avg=$(echo "scale=3; $sum/$n" | bc -l)
+
+len=${#arr[@]}
+avg=$(echo "scale=3; $sum/$len" | bc -l)
 echo "Average is: $avg"
